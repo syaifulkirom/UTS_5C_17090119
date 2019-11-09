ae56118d0d2b92a4f8cbeaa9a6c3b50acce81902
syaifulkirom <49852040+syaifulkirom@users.noreply.github.com>
2019-11-09 19:42:13 +0700

    Add files via upload

---
+++ include/header.php
@@ -0,0 +1,14 @@
+<meta name="msapplication-TileColor" content="#663fb5">
+<meta name="msapplication-TileImage" content="home/img/favicon/mstile-144x144.png">
+<meta name="msapplication-config" content="home/img/favicon/browserconfig.xml">
+<meta name="theme-color" content="#663fb5">
+
+
+<link rel="stylesheet" type="text/css" href="./asset/css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="./asset/css/dataTables.bootstrap.min.css"/>
+<link rel="stylesheet" type="text/css" href="./asset/sweetalert/dist/sweetalert.css">
+<script src="./asset/js/jquery.min.js" ></script>
+<script src="./asset/js/bootstrap.min.js" ></script>
+<script src="./asset/js/datatables.min.js" ></script>
+<script src="./asset/js/dataTables.bootstrap.min.js" ></script>
+<script type="text/javascript" src="./asset/sweetalert/dist/sweetalert.min.js"></script>
